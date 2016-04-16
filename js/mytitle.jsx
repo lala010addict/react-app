4a2651f2446e94b7d5a97f83e9bee8ea9195d474
jieyingpeng <jipeng@syr.edu>
2016-04-16 16:41:09 -0700

    changing to jsx

--- js/mytitle.jsx
+++ js/mytitle.jsx
@@ -1,12 +1,16 @@
 var React = require('react')
 var div = React.DOM.div
 var h1 = React.DOM.h1
+
+
 var myTitle = React.createClass({
     render: function() {
         return (
-            div(null,
-                h1({ style: { color: this.props.color } }, this.props.title)
-            )
+           <div>
+                <h1 style= { color: this.props.color }>
+                 {this.props.title}
+                </h1>
+           </div>
         )
     }
 })
