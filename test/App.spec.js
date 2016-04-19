7cba34c1c668540d3e5299ff8af6dae509ac3da1
jieyingpeng <jipeng@syr.edu>
2016-04-19 16:19:24 -0700

    test search

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,12 +4,46 @@ const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
 const { shallow } = require('enzyme')
-
+const ShowCard = require('../js/ShowCard')
+const { shows } = require('../public/data')
 describe('<Search />', () => {
-            it('should render the brand', () => {
-                // expect(1 + 1 === 2).to.be.true
-                const wrapper = shallow( < Search / > )
-                    //is the brand getting rendered
-                expect(wrapper.contains( < h1 className = 'brand' > svideo < /h1>)).to.be.true
-                })
+     it('should render the brand', () => {
+	                // expect(1 + 1 === 2).to.be.true
+	                const wrapper = shallow( <Search / > )
+	               // console.log(wrapper.debug())
+	                    //is the brand getting rendered
+	                expect(wrapper.contains( <h1 className = 'brand' >svideo</h1>)).to.be.true
+	                })	   
+
+
+     it('should render as many shows', () =>{
+     	const wrapper = shallow(<Search />)
+     	expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+
+
+
+
+     })
+
+
+
+
+
+
             })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
