06a814b18c5f606edd65eb5342898c63eb656b4b
jieyingpeng <jipeng@syr.edu>
2016-04-19 16:01:43 -0700

    testing

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,15 @@
 /* eslint-env mocha */
 
-const {expect} = require('chai')
+const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const { shallow } = require('enzyme')
 
 describe('<Search />', () => {
-	it('should pass', () => {
-		expect(1+1 === 2).to.be.true
-	})
-})
+            it('should render the brand', () => {
+                // expect(1 + 1 === 2).to.be.true
+                const wrapper = shallow( < Search / > )
+                    //is the brand getting rendered
+                expect(wrapper.contains( < h1 className = 'brand' > svideo < /h1>)).to.be.true
+                })
+            })
